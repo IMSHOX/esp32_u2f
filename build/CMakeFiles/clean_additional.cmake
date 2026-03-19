83fcbcc38c5031dd3abd689ecf52d93a50213f3d
imadox <imad2010j@gmail.com>
2026-03-19 16:25:16 +0100

    first

---
+++ build/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,27 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
+  file(REMOVE_RECURSE
+  "bootloader/bootloader.bin"
+  "bootloader/bootloader.elf"
+  "bootloader/bootloader.map"
+  "config/sdkconfig.cmake"
+  "config/sdkconfig.h"
+  "esp-idf/esptool_py/flasher_args.json.in"
+  "esp-idf/mbedtls/x509_crt_bundle"
+  "esp32_u2f.bin"
+  "esp32_u2f.map"
+  "flash_app_args"
+  "flash_bootloader_args"
+  "flash_project_args"
+  "flasher_args.json"
+  "ldgen_libraries"
+  "ldgen_libraries.in"
+  "project_elf_src_esp32s3.c"
+  "u2f_aaguid.bin.S"
+  "u2f_cert.bin.S"
+  "u2f_cert_key.bin.S"
+  "x509_crt_bundle.S"
+  )
+endif()
---
+++ build/esp-idf/mbedtls/mbedtls/library/error.c
@@ -0,0 +1,1 @@
+/home/imadox/esp/esp-idf/components/mbedtls/mbedtls/library/error.c
